b6d95313c23c4b03d97176db1d700a76c3a4462c
GMLaasya <23211a0586@bvrit.ac.in>
2026-05-14 19:43:15 +0530

    Removed contact section image

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -55,12 +55,9 @@ export default function Contact() {
           <div className="contact-image-div">
             {illustration.animated ? (
               <DisplayLottie animationData={email} />
-            ) : (
-              <img
-                alt="Man working"
-                src={require("../../assets/images/contactMailDark.svg")}
-              ></img>
-            )}
+            ) 
+          
+            }
           </div>
         </div>
       </div>
